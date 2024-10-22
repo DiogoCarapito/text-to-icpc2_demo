ffeeea815497697b4f1bdef5556c94200a5fb988
DiogoCarapito <diogo.carapito@icloud.com>
2024-10-22 18:33:24 +0100

    new model avalibalble

--- sections/demo.py
+++ sections/demo.py
@@ -25,7 +25,8 @@ def demo():
     # model load and pipeline creation
     
     # choose model
-    model_chosen = st.radio("Escolha o modelo", ("text-to-icpc2", "text-to-icpc2-bert-base-uncased"))
+    #model_chosen = "text-to-icpc2"
+    model_chosen = "text-to-icpc2-bert-base-uncased"
     
     pipe = load_model(f"diogocarapito/{model_chosen}", available_device)
 
