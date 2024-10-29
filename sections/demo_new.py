d2b250141846681882d0756b856405a267315d85
DiogoCarapito <diogo.carapito@icloud.com>
2024-10-29 12:43:25 +0000

    added regex to filter codes on text input and search if code exists instead of using the model to predict the text

--- sections/demo_new.py
+++ sections/demo_new.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 from supabase import create_client, Client
 from datetime import datetime
 
+import re
+
 
 def load_supabase():
     # load .env file
@@ -72,86 +74,102 @@ def demo_new():
     # text input
     text = st.text_input("Coloque um diagnóstico para o modelo classificar")
 
-    if text != "":
-        # Record the start time
-        start_time = timeit.default_timer()
+    lables_dataframe = pd.read_csv(
+        "https://raw.githubusercontent.com/DiogoCarapito/text-to-icpc2/main/data/icpc2_processed.csv"
+    )
 
-        # Execute prediction
-        predictions = pipe(text)
+    lable_code_dict = pd.read_csv(
+        "https://raw.githubusercontent.com/DiogoCarapito/text-to-icpc2/refs/heads/main/data/code_text_label.csv"
+    )
 
-        # Record the end time
-        end_time = timeit.default_timer()
-
-        # Calculate the elapsed time
-        elapsed_time = end_time - start_time
-
-        # Display the elapsed time
-        st.write(
-            f"Tempo necessário para classificação: **{elapsed_time:.4f} segundos** com **'{available_device}'**"
-        )
-
-        lables_dataframe = pd.read_csv(
-            "https://raw.githubusercontent.com/DiogoCarapito/text-to-icpc2/main/data/icpc2_processed.csv"
-        )
-
-        lable_code_dict = pd.read_csv(
-            "https://raw.githubusercontent.com/DiogoCarapito/text-to-icpc2/refs/heads/main/data/code_text_label.csv"
-        )
-
-        # st.write(lable_code_dict)
-
-        for each in predictions:
-            # transform the lable into a icpc2 code
-            if each["label"].startswith("LABEL_"):
-                # split on the "_" and get the 2nd part and make it integer
-                each["label"] = int(each["label"].split("_")[1])
-
-                # get the code
-                each["code"] = lable_code_dict.loc[
-                    lable_code_dict["label"] == each["label"], "code"
-                ].values[0]
-
-                # get the desctição
-                each["text"] = lable_code_dict.loc[
-                    lable_code_dict["label"] == each["label"], "text"
-                ].values[0]
-
-        if "copy" not in st.session_state:
-            st.session_state["copy"] = False
-
-        if "feedback" not in st.session_state:
-            st.session_state["feedback"] = None
-
-        st.write("")
-        st.subheader("O modelo portou-se bem?")
-        
-        col_1, col_2, col_3 = st.columns([1,1,1])
-
-        with col_1:
-            if st.button("Copiar código", type="primary"):
-                pyperclip.copy(predictions[0]["code"])
-                st.session_state["copy"] = True
-
-        with col_2:
-            if st.button("👍", type="primary"):
-                st.session_state["feedback"] = 1
-
-        with col_3:
-            if st.button("👎", type="secondary"):
-                st.session_state["feedback"] = -1
-        st.write("")
-        # text_input, predicted_code, predicted_lable, model
-        prediction_display(predictions, lables_dataframe)
-        
-        supabase_insert(
-            text,  # text_input
-            predictions[0]["text"],  # predicted_text
-            predictions[0]["code"],  # predicted_code
-            predictions[0]["label"],  # predicted_lable
-            model_chosen,  # model
-            st.session_state["feedback"],
-            st.session_state["copy"],
-        )
+    if text == "":
+        st.warning("Coloque um diagnóstico para classificar")
 
     else:
-        st.warning("Coloque um diagnóstico para classificar")
+        # check if text corresponds to a letter and 2 algarisms with re
+        if bool(re.match(r"[A-Za-z]\d\d", text)):
+            # make the text uppercase
+            text = text.upper()
+
+            try:
+                # get the code
+                description = lables_dataframe.loc[
+                    lables_dataframe["cod"] == text, "nome"
+                ].values[0]
+
+                st.write(f"## {text} - {description}")
+            except IndexError:
+                st.warning("Código não encontrado")
+
+        else:
+            # Record the start time
+            start_time = timeit.default_timer()
+
+            # Execute prediction
+            predictions = pipe(text)
+
+            # Record the end time
+            end_time = timeit.default_timer()
+
+            # Calculate the elapsed time
+            elapsed_time = end_time - start_time
+
+            # Display the elapsed time
+            st.write(
+                f"Tempo necessário para classificação: **{elapsed_time:.4f} segundos** com **'{available_device}'**"
+            )
+
+            # st.write(lable_code_dict)
+
+            for each in predictions:
+                # transform the lable into a icpc2 code
+                if each["label"].startswith("LABEL_"):
+                    # split on the "_" and get the 2nd part and make it integer
+                    each["label"] = int(each["label"].split("_")[1])
+
+                    # get the code
+                    each["code"] = lable_code_dict.loc[
+                        lable_code_dict["label"] == each["label"], "code"
+                    ].values[0]
+
+                    # get the desctição
+                    each["text"] = lable_code_dict.loc[
+                        lable_code_dict["label"] == each["label"], "text"
+                    ].values[0]
+
+            if "copy" not in st.session_state:
+                st.session_state["copy"] = False
+
+            if "feedback" not in st.session_state:
+                st.session_state["feedback"] = None
+
+            st.write("")
+            st.subheader("O modelo portou-se bem?")
+
+            col_1, col_2, col_3 = st.columns([1, 1, 1])
+
+            with col_1:
+                if st.button("Copiar código", type="primary"):
+                    pyperclip.copy(predictions[0]["code"])
+                    st.session_state["copy"] = True
+
+            with col_2:
+                if st.button("👍", type="primary"):
+                    st.session_state["feedback"] = 1
+
+            with col_3:
+                if st.button("👎", type="secondary"):
+                    st.session_state["feedback"] = -1
+            st.write("")
+            # text_input, predicted_code, predicted_lable, model
+            prediction_display(predictions, lables_dataframe)
+
+            supabase_insert(
+                text,  # text_input
+                predictions[0]["text"],  # predicted_text
+                predictions[0]["code"],  # predicted_code
+                predictions[0]["label"],  # predicted_lable
+                model_chosen,  # model
+                st.session_state["feedback"],
+                st.session_state["copy"],
+            )
